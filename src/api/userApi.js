d3ea09dc1c34ed7085bf09e331478da5cd9e60d0
unknown <varshinikura267@gmail.com>
2026-06-27 20:50:17 +0530

    Configure user API service

---
+++ src/api/userApi.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const API_URL = "https://jsonplaceholder.typicode.com/users";
+
+export const getUsers = async () => {
+  const response = await axios.get(API_URL);
+  return response.data;
+};
+
+export const addUser = async (user) => {
+  const response = await axios.post(API_URL, user);
+  return response.data;
+};
+
+export const updateUser = async (id, user) => {
+  const response = await axios.put(`${API_URL}/${id}`, user);
+  return response.data;
+};
+
+export const deleteUser = async (id) => {
+  const response = await axios.delete(`${API_URL}/${id}`);
+  return response.data;
+};
