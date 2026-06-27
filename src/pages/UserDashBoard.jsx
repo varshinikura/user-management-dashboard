1cd7d1ff3f35ea0e4d2801b4a74115c46b5d8a64
unknown <varshinikura267@gmail.com>
2026-06-27 23:26:14 +0530

    Connect user form to dashboard

--- src/pages/UserDashBoard.jsx
+++ src/pages/UserDashBoard.jsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
+import UserForm from "../components/UserForm";
+
 function UserDashboard() {
+  const [selectedUser, setSelectedUser] = useState(null);
+
+  const handleSubmit = (user) => {
+    console.log(user);
+  };
+
   return (
     <div className="container">
       <h1>User Management Dashboard</h1>
+
+      <UserForm
+        selectedUser={selectedUser}
+        onSubmit={handleSubmit}
+        onCancel={() => setSelectedUser(null)}
+      />
     </div>
   );
 }
